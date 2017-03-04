a3f9c57eb4020dcfd6e2d8703a24c76d0c06fad5
neo <neolithic1228@outlook.com>
2017-03-04 23:44:03 +0800

    测试一下SourceTree

---
+++ JingLingLeague/src/main/java/cn/edu/jlxy/jinglingleague/entity/PlayerSpec.java
@@ -0,0 +1,96 @@
+package cn.edu.jlxy.jinglingleague.entity;
+
+/**
+ * Created by neo on 2017/3/4.
+ */
+public class PlayerSpec {
+    private int pId;
+
+    private String pHeight;
+
+    private String pWeight;
+
+    private String pStrong;
+
+    private String pStamina;
+
+    private String pSpeed;
+
+    private String pShoot;
+
+    private String pSkill;
+
+    private String pdefence;
+
+    public int getpId() {
+        return pId;
+    }
+
+    public void setpId(int pId) {
+        this.pId = pId;
+    }
+
+    public String getpHeight() {
+        return pHeight;
+    }
+
+    public void setpHeight(String pHeight) {
+        this.pHeight = pHeight;
+    }
+
+    public String getpWeight() {
+        return pWeight;
+    }
+
+    public void setpWeight(String pWeight) {
+        this.pWeight = pWeight;
+    }
+
+    public String getpStrong() {
+        return pStrong;
+    }
+
+    public void setpStrong(String pStrong) {
+        this.pStrong = pStrong;
+    }
+
+    public String getpStamina() {
+        return pStamina;
+    }
+
+    public void setpStamina(String pStamina) {
+        this.pStamina = pStamina;
+    }
+
+    public String getpSpeed() {
+        return pSpeed;
+    }
+
+    public void setpSpeed(String pSpeed) {
+        this.pSpeed = pSpeed;
+    }
+
+    public String getpShoot() {
+        return pShoot;
+    }
+
+    public void setpShoot(String pShoot) {
+        this.pShoot = pShoot;
+    }
+
+    public String getpSkill() {
+        return pSkill;
+    }
+
+    public void setpSkill(String pSkill) {
+        this.pSkill = pSkill;
+    }
+
+    public String getPdefence() {
+        return pdefence;
+    }
+
+    public void setPdefence(String pdefence) {
+        this.pdefence = pdefence;
+    }
+}
