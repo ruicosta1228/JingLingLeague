036342b84ae327ba9a758cdcf13eb292392cf4ba
neo <neolithic1228@outlook.com>
2017-03-05 12:26:59 +0800

    iOS结构框架,设置启动图片和图标

---
+++ 金陵联赛/金陵联赛/Classes/View/Discovery(约球)/JLLDiscoveryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JLLDiscoveryViewController.swift
+//  金陵联赛
+//
+//  Created by neo on 2017/3/5.
+//  Copyright © 2017年 neo. All rights reserved.
+//
+
+import UIKit
+
+class JLLDiscoveryViewController: JLLBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 金陵联赛/金陵联赛/Classes/View/Main/JLLMainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  JLLMainViewController.swift
+//  金陵联赛
+//
+//  Created by neo on 2017/3/5.
+//  Copyright © 2017年 neo. All rights reserved.
+//
+
+import UIKit
+
+class JLLMainViewController: UITabBarController {
+
+    
+
+}
