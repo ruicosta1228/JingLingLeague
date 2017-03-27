13c36b9a1e3418ca8c08fa0d31335113e41e9c4c
Salasoul <525743582@qq.com>
2017-03-27 21:21:15 +0800

    xib文件

---
+++ 金陵联赛/金陵联赛/Classes/View/Society(社区)/StatusCell/JLLStatusCell.swift
@@ -0,0 +1,24 @@
+ //
+//  JLLStatusCell.swift
+//  金陵联赛
+//
+//  Created by Salasoul on 2017/3/27.
+//  Copyright © 2017年 neo. All rights reserved.
+//
+
+import UIKit
+
+class JLLStatusCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
