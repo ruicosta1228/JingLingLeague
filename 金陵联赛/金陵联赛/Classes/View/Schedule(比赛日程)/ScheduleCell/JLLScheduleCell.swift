61099391b3554455d4432ca35fe5834edcdbeeb6
Salasoul <525743582@qq.com>
2017-03-27 22:54:05 +0800

    16-用户模型 & 比赛cell xib

---
+++ 金陵联赛/金陵联赛/Classes/View/Schedule(比赛日程)/ScheduleCell/JLLScheduleCell.swift
@@ -0,0 +1,24 @@
+//
+//  JLLScheduleCell.swift
+//  金陵联赛
+//
+//  Created by Salasoul on 2017/3/27.
+//  Copyright © 2017年 neo. All rights reserved.
+//
+
+import UIKit
+
+class JLLScheduleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ 金陵联赛/金陵联赛/Classes/View/Society(社区)/StatusCell/JLLUser.swift
@@ -0,0 +1,23 @@
+//
+//  JLLUser.swift
+//  金陵联赛
+//
+//  Created by Salasoul on 2017/3/27.
+//  Copyright © 2017年 neo. All rights reserved.
+//
+
+import UIKit
+
+//用户模型
+class JLLUser: NSObject {
+    //用户id
+    var id: Int64 = 0
+    //用户昵称
+    var name: String?
+    //用户头像地址
+    var image_url: String?
+    
+//    override var description: String{
+//        return
+//    }
+}
