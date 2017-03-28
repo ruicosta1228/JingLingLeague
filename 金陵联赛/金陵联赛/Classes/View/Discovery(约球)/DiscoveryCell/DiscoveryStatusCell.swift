a3e83c533646121b347a2c45b2813de89ef4281b
Salasoul <525743582@qq.com>
2017-03-29 03:14:26 +0800

    加入比赛添加时间戳

--- 金陵联赛/金陵联赛/Classes/View/Discovery(约球)/DiscoveryCell/DiscoveryStatusCell.swift
+++ 金陵联赛/金陵联赛/Classes/View/Discovery(约球)/DiscoveryCell/DiscoveryStatusCell.swift
@@ -16,6 +16,9 @@ class DiscoveryStatusCell: UITableViewCell {
     // 加入比赛 按钮
     @IBOutlet weak var joinBtn: UIButton!
     
+    //比赛发起时间
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
