24328444e4208822344010a918c669f86248f7ab
Salasoul <525743582@qq.com>
2017-03-30 22:57:03 +0800

    导入LTMorphingLabel库

--- 金陵联赛/Pods/Target Support Files/Pods-金陵联赛/Pods-金陵联赛-frameworks.sh
+++ 金陵联赛/Pods/Target Support Files/Pods-金陵联赛/Pods-金陵联赛-frameworks.sh
@@ -91,6 +91,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FMDB/FMDB.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/LTMorphingLabel/LTMorphingLabel.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SVProgressHUD/SVProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/YYModel/YYModel.framework"
@@ -99,6 +100,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FMDB/FMDB.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/LTMorphingLabel/LTMorphingLabel.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SVProgressHUD/SVProgressHUD.framework"
   install_framework "$BUILT_PRODUCTS_DIR/YYModel/YYModel.framework"
---
+++ 金陵联赛/金陵联赛/Classes/View/NewFeature(欢迎)/JLLWelcomeView.swift
@@ -0,0 +1,24 @@
+//
+//  JLLWelcomeView.swift
+//  金陵联赛
+//
+//  Created by Salasoul on 2017/3/30.
+//  Copyright © 2017年 neo. All rights reserved.
+//
+
+import UIKit
+
+class JLLWelcomeView: UIView {
+
+    //显示文字
+    @IBOutlet weak var textLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
