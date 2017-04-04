1ee40177012a94601e5d8cf5cc55098e13828bdc
Marcowlx <568209852@qq.com>
2017-04-04 14:34:37 +0800

    更新个人界面 将柱状图换为饼图

--- 金陵联赛/金陵联赛/Classes/View/Profile(个人)/JLLProfileViewController.swift
+++ 金陵联赛/金陵联赛/Classes/View/Profile(个人)/JLLProfileViewController.swift
@@ -10,20 +10,24 @@ import UIKit
 import Charts
 class JLLProfileViewController: JLLBaseViewController {
 
+    
     @IBOutlet weak var radarChartView: RadarChartView!
-    @IBOutlet weak var barChartView: BarChartView!
+    @IBOutlet weak var pieChartView: PieChartView!
+//    @IBOutlet weak var barChartView: BarChartView!
+    
    
     var months:[String]!
     var abi:[String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        months = ["Jan", "Feb", "Mar", "Apr", "May"]
-
-        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0]
-        
-        setChart(dataPoints: months, values: unitsSold)
+//        months = ["Jan", "Feb", "Mar", "Apr", "May"]
+//
+//        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0]
+//        
+//        setChart(dataPoints: months, values: unitsSold)
         setRadar()
+        setPie()
    
         
         
@@ -35,26 +39,28 @@ class JLLProfileViewController: JLLBaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func setChart(dataPoints: [String], values: [Double]) {
-        barChartView.noDataText = "You need to provide data for the chart."
-        var dataEntries: [BarChartDataEntry] = []
-        
-        for i in 0..<dataPoints.count {
-            let dataEntry = BarChartDataEntry(x: Double(i), yValues: [values[i]])
-            dataEntries.append(dataEntry)
-        }
-        
-        let chartDataSet = BarChartDataSet(values: dataEntries, label: "能力值")
-        let chartData = BarChartData(dataSet: chartDataSet)
-        barChartView.data = chartData
-        chartDataSet.colors = [UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)]
-        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: .easeInBounce)
-        
-        //使x轴位于下方
-        barChartView.xAxis.labelPosition = .bottom
-
-        
-    }
+//    func setChart(dataPoints: [String], values: [Double]) {
+//        barChartView.noDataText = "You need to provide data for the chart."
+//        var dataEntries: [BarChartDataEntry] = []
+//        
+//        for i in 0..<dataPoints.count {
+//            let dataEntry = BarChartDataEntry(x: Double(i), yValues: [values[i]])
+//            dataEntries.append(dataEntry)
+//        }
+//        
+//        let chartDataSet = BarChartDataSet(values: dataEntries, label: "能力值")
+//        let chartData = BarChartData(dataSet: chartDataSet)
+//        barChartView.data = chartData
+//        chartDataSet.colors = [UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)]
+//        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: .easeInBounce)
+//        
+//        //使x轴位于下方
+//        barChartView.xAxis.labelPosition = .bottom
+//
+//        
+//    }
+    
+    //雷达图显示方法
     
     func setRadar(){
         
@@ -85,10 +91,40 @@ class JLLProfileViewController: JLLBaseViewController {
         radarChartView.xAxis.axisMinimum = 0.0    //最小值
 //        radarChartView.yAxis.drawAxisLineEnabled = false
 
-        
+        radarChartView.chartDescription?.text = "Ability Radar Chart"
         radarChartView.animate(xAxisDuration: 0.0, yAxisDuration: 2.0, easingOption: .easeInBounce)
         
     }
+    
+    //饼图显示方法
+    func setPie(){
+        let ys1 = Array(1..<6).map { x in return sin(Double(x) / 2.0 / 3.141 * 1.5) * 100.0 }
+        
+        let yse1 = ys1.enumerated().map { x, y in return PieChartDataEntry(value: y, label: String(x)) }
+        
+        let data = PieChartData()
+        let ds1 = PieChartDataSet(values: yse1, label: "能力值")
+        
+        ds1.colors = ChartColorTemplates.vordiplom()
+        
+        data.addDataSet(ds1)
+        
+        let paragraphStyle: NSMutableParagraphStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        paragraphStyle.lineBreakMode = .byTruncatingTail
+        paragraphStyle.alignment = .center
+        let centerText: String =  "by Marco"
+//      设置中间文字属性
+        
+        
+//        centerText.setAttributes([NSFontAttributeName: UIFont(name: "HelveticaNeue-Light", size: 15.0)!, NSParagraphStyleAttributeName: paragraphStyle], range: NSMakeRange(0, centerText.length))
+//        centerText.addAttributes([NSFontAttributeName: UIFont(name: "HelveticaNeue-Light", size: 13.0)!, NSForegroundColorAttributeName: UIColor.gray], range: NSMakeRange(10, centerText.length - 10))
+//        centerText.addAttributes([NSFontAttributeName: UIFont(name: "HelveticaNeue-LightItalic", size: 13.0)!, NSForegroundColorAttributeName: UIColor(red: 51 / 255.0, green: 181 / 255.0, blue: 229 / 255.0, alpha: 1.0)], range: NSMakeRange(centerText.length - 19, 19))
+        
+        pieChartView.centerText = centerText
+        pieChartView.animate(xAxisDuration: 0.0, yAxisDuration: 2.0, easingOption: .easeInBounce)
+        pieChartView.data = data
+        
+        pieChartView.chartDescription?.text = "Ability Pie Chart"    }
     
     override func setupTableView() {
         self.view.backgroundColor = UIColor.white
