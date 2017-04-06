9ece199eb325ff0d4f52e7b4708199a607ccfd71
Salasoul <525743582@qq.com>
2017-04-06 17:57:19 +0800

    图片九宫格布局(等model写完 修改fixme)

--- 金陵联赛/金陵联赛/Classes/View/Society(社区)/StatusCell/JLLStatusPictureView.swift
+++ 金陵联赛/金陵联赛/Classes/View/Society(社区)/StatusCell/JLLStatusPictureView.swift
@@ -11,5 +11,48 @@ import UIKit
 class JLLStatusPictureView: UIView {
 
     @IBOutlet weak var heightCons: NSLayoutConstraint!
+    
+    override func awakeFromNib() {
+        setupUI()
+    }
 
+}
+
+
+extension JLLStatusPictureView {
+    func setupUI() {
+        
+        //超出边界的内容不显示
+        clipsToBounds = true
+        
+        let count = 3
+        
+        //FIXME-修改每个imageView的位置
+        let rect = CGRect(x: 0,
+                          y: 100,
+                          width: 100,
+                          height: 100)
+        
+        //循环创建9个imageView
+        for i in 0..<count * count {
+            
+            let iv = UIImageView()
+            
+            iv.backgroundColor = UIColor.blue
+            
+            //行
+            let row = CGFloat(i / count)
+            //列
+            let col = CGFloat(i % count)
+            
+            //FIXME
+            let xOffset = col //* (JLLStatusPictureItemWidth + JLLStatusPictureInnerMargin)
+            let yOffset = row //* (JLLStatusPictureItemWidth + JLLStatusPictureInnerMargin)
+            
+            iv.frame = rect.offsetBy(dx: xOffset, dy: yOffset)
+            
+            addSubview(iv)
+        }
+        
+    }
 }
