cdbc67cdc5231cad51ca3302133bcdb43068bfc6
Salasoul <525743582@qq.com>
2017-04-06 11:15:00 +0800

    评论和赞 按钮连线

---
+++ 金陵联赛/金陵联赛/Classes/View/Society(社区)/StatusCell/JLLStatusToolBar.swift
@@ -0,0 +1,19 @@
+//
+//  JLLStatusToolBar.swift
+//  金陵联赛
+//
+//  Created by Salasoul on 2017/4/6.
+//  Copyright © 2017年 neo. All rights reserved.
+//
+
+import UIKit
+
+class JLLStatusToolBar: UIView {
+
+    //评论
+    @IBOutlet weak var commentBtn: UIButton!
+    
+    //赞
+    @IBOutlet weak var likeBtn: UIButton!
+
+}
