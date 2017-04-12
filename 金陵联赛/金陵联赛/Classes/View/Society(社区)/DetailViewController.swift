bd64edfc015bb8fe4708c99a2c97ec88e4953f04
Salasoul <525743582@qq.com>
2017-04-12 21:00:44 +0800

    detail显示xib

--- 金陵联赛/金陵联赛/Classes/View/Society(社区)/DetailViewController.swift
+++ 金陵联赛/金陵联赛/Classes/View/Society(社区)/DetailViewController.swift
@@ -33,6 +33,8 @@ class DetailViewController: JLLBaseViewController {
     
     @IBOutlet weak var commentName: UILabel!
     
+    @IBOutlet weak var commentTime: UILabel!
+    
     @IBOutlet weak var contentLabel: UILabel!
     
     
@@ -40,7 +42,19 @@ class DetailViewController: JLLBaseViewController {
         
         super.viewDidLoad()
         
-        setupTableView()
+        //设置空间具体内容
+        titleLabel.text = "内马尔"
+        userImage.image = UIImage(named: "Neymar")
+        userName.text = "内马尔"
+        composeTime.text = "20:52"
+        subtitle.text = "巴塞罗那内马尔"
+        statusLabel.text = "我是巴塞罗那的内马尔，我也不知道写什么，那就......哈哈哈哈"
+        composeImage.image = UIImage(named: "Neymar_full")
+        likedUsers.image = UIImage(named: "Neymar")
+        commentUser.image = UIImage(named: "Barca")
+        commentName.text = "巴塞罗那"
+        commentTime.text = "10分钟前"
+        contentLabel.text = "哈哈哈哈哈"
         
     }
 
@@ -48,17 +62,44 @@ class DetailViewController: JLLBaseViewController {
         super.didReceiveMemoryWarning()
         
     }
+
     
-    override func awakeFromNib() {
-        composeImage.image = UIImage(named: "Neymar_full")
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: "View", bundle: nil)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
 }
 
 extension DetailViewController {
     override func setupTableView() {
-//        self.view.backgroundColor = UIColor.white
+//        self.view.backgroundColor = UIColor.black
         
+        //底部工具栏
+        let commentView = UIView()
+        commentView.frame = CGRect(x: 0, y: self.view.frame.height - 50, width: self.view.frame.width, height: 50)
+//        commentView.backgroundColor = UIColor.blue
+        self.view.addSubview(commentView)
         
+        //文本输入框
+        let text = UITextField()
+        text.frame = CGRect(x: 10, y: 5, width: self.view.frame.width - 80, height: 40)
+//        text.borderStyle = UITextBorderStyle.roundedRect
+        text.layer.borderWidth = 2
+        text.layer.cornerRadius = 20
+        text.placeholder = "  发表评论"
+        
+        //表情按钮
+        let couBtn = UIButton.init(type: UIButtonType.custom) as UIButton
+        let smile = UIImage(named: "bq")
+        couBtn.setImage(smile, for: UIControlState.normal)
+        couBtn.frame = CGRect(x: self.view.frame.width - 55, y: 5, width: 40, height: 40)
+        
+        commentView.addSubview(text)
+        commentView.addSubview(couBtn)
     }
 }
