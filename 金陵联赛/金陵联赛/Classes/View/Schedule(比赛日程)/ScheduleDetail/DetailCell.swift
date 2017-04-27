e9a2ed9b9fe08a112b936075943cf47e23aa83bb
Marcowlx <568209852@qq.com>
2017-04-27 17:09:09 +0800

    detail实现局部滚动

---
+++ 金陵联赛/金陵联赛/Classes/View/Schedule(比赛日程)/ScheduleDetail/DetailCell.swift
@@ -0,0 +1,27 @@
+//
+//  DetailCell.swift
+//  金陵联赛
+//
+//  Created by Marco on 2017/4/27.
+//  Copyright © 2017年 neo. All rights reserved.
+//
+
+import UIKit
+
+class DetailCell: UITableViewCell {
+
+    
+    @IBOutlet weak var cellimage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- 金陵联赛/金陵联赛/Classes/View/Schedule(比赛日程)/ScheduleDetail/JLLScheduleDetailViewController.swift
+++ 金陵联赛/金陵联赛/Classes/View/Schedule(比赛日程)/ScheduleDetail/JLLScheduleDetailViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
+private let cellId = "cellId"
+
+let ability: [String] = ["1","2","3"]
 class JLLScheduleDetailViewController: JLLBaseViewController {
+    @IBOutlet weak var tabview: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +25,8 @@ class JLLScheduleDetailViewController: JLLBaseViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
-    override func setupTableView() {
-        self.view.backgroundColor = UIColor.white
+    override func loadData() {
+        print("加载数据")
     }
     /*
     // MARK: - Navigation
@@ -36,3 +39,52 @@ class JLLScheduleDetailViewController: JLLBaseViewController {
     */
 
 }
+
+extension JLLScheduleDetailViewController{
+    
+    override func setupTableView() {
+        super.setupTableView()
+        self.view.backgroundColor = UIColor.white
+        tableView?.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: tabBarController?.tabBar.bounds.height ?? 49, right: 0)
+        navigationBar.isHidden = true
+        //注册原型cell
+        tableView?.register(UINib(nibName: "DetailCell", bundle: nil), forCellReuseIdentifier: cellId)
+        
+        //设置行高
+        tableView?.rowHeight = 550
+        tableView?.estimatedRowHeight = 600
+        
+        //取消分割线
+        tableView?.separatorStyle = .none
+        
+        tabview.addSubview(tableView!)
+        
+    }
+}
+
+//具体数据源方法实现
+extension JLLScheduleDetailViewController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ability.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! DetailCell
+        if indexPath.row == 0{
+            cell.cellimage.image = UIImage(named: "Schedule")
+        }
+        if indexPath.row == 1 {
+            cell.cellimage.image = UIImage(named: "Schedule1")
+        }
+        if indexPath.row == 2 {
+            cell.cellimage.image = UIImage(named: "Schedule2")
+        }
+        return cell
+    }
+    
+    
+    
+}
+
+
+
